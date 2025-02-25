cc04758ce1a5eec39f8cd37899ba42afb477cb88
jglepko <36523615+jglepko@users.noreply.github.com>
2025-02-25 14:45:52 -0500

    Update matching_engine.h

--- Chapter6/exchange/matcher/matching_engine.h
+++ Chapter6/exchange/matcher/matching_engine.h
@@ -27,3 +27,65 @@ namespace Exchange {
       auto order_book = ticker_order_book_[client_request->ticker_id_];
       switch (client_request->type_) {
         case ClientRequestType::NEW: {
+          order_book->add(client_request->client_id_, client_request->order_id_, client_request->ticker_id_,
+                          client_request->side_, client_request->price_, client_request->qty_);
+        }
+          break;
+
+        default: {
+          FATAL("Received invalid client-request-type:" + clientRequestTypeToString(client_request->type_));
+        }
+          break;
+      }
+    }
+
+    auto sendClientResponse(const MEClientResponse *client_response) noexcept {
+      logger_.log("%:% %() % Sending %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_), client_response->toString());
+      auto next_write = outgoing_ogw_responses_->getNextToWriteTo();
+      *next_write = std::move(*client_response);
+      outgoing_ogw_responses_->updateWriteIndex();
+    }
+
+    auto sendMarketUpdate(const MEMarketUpdate *market_update) noexcept {
+      logger_.log("%:% %() % Sending %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_), market_update->toString());
+      auto next_write = outgoing_md_updates_->getNextToWriteTo();
+      *next_write = *market_update;
+      outgoing_md_updates_->updateWriteIndex();
+    }
+
+    auto run() noexcept {
+      logger_.log("%:% %() %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_));
+      while (run_) {
+        const auto me_client_request = incoming_requests_->getNextToRead();
+        if (LIKELY(me_client_request)) {
+          logger_.log("%:% %() % Processing %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_),
+                      me_client_request->toString());
+          processClientRequest(me_client_request);
+          incoming_requests_->updateReadIndex();
+        }
+      }
+    }
+
+    MatchingEngine() = delete;
+
+    MatchingEngine(const MatchingEngine &) = delete;
+
+    MatchingEngine(const MatchingEngine &&) = delete;
+
+    MatchingEngine &operator=(const MatchingEngine &) = delete;
+
+    MatchingEngine &operator=(const MatchingEngine &&) = delete;
+
+  private:
+    OrderBookHashMap ticker_order_book_;
+
+    ClientRequestLFQueue *incoming_requests_ = nullptr;
+    ClientResponseLFQueue *outgoing_ogw_responses_ = nullptr;
+    MEMarketUpdateLFQueue *outgoing_md_updates_ = nullptr;
+
+    volatile bool run_ = false;
+
+    std::string time_str_;
+    Logger logger_;
+  };
+}
