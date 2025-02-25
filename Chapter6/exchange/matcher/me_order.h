c8c4e566bc656eb78605c5c6a59b05c31561b81d
jglepko <36523615+jglepko@users.noreply.github.com>
2025-02-25 13:59:17 -0500

    Update me_order.h

--- Chapter6/exchange/matcher/me_order.h
+++ Chapter6/exchange/matcher/me_order.h
@@ -28,28 +28,37 @@ namespace Exchange {
           side_(side), price_(price), qty_(qty), priority_(priority), prev_order_(prev_order), next_order_(next_order) {}
 
     auto toString() const -> std::string;
-};
+  };
 
-typedef std::array<MEOrder *, ME_MAX_ORDER_IDS> OrderHashMap;
-typedef std::array<OrderHashMap, ME_MAX_NUM_CLIENTS> ClientOrderHashMap;
+  typedef std::array<MEOrder *, ME_MAX_ORDER_IDS> OrderHashMap;
+  typedef std::array<OrderHashMap, ME_MAX_NUM_CLIENTS> ClientOrderHashMap;
 
-struct MEOrdersAtPrice {
-  Side side_ = Side::INVALID;
-  Price price_ = Price_INVALID;
+  struct MEOrdersAtPrice {
+    Side side_ = Side::INVALID;
+    Price price_ = Price_INVALID;
 
-  MEOrder *first_me_order_ = nullptr;
+    MEOrder *first_me_order_ = nullptr;
 
-  MEOrdersAtPrice *prev_entry_ = nullptr;
-  MEOrdersAtPrice *next_entry_ = nullptr;
+    MEOrdersAtPrice *prev_entry_ = nullptr;
+    MEOrdersAtPrice *next_entry_ = nullptr;
 
-  MEOrdersAtPrice() = default;
+    MEOrdersAtPrice() = default;
 
-  MEOrdersAtPrice(Side side, Price price,  MEOrder *first_me_order, MEOrdersAtPrice *prev_entry, MEOrdersAtPrice *next_entry)
-    : side_(side), price_(price), first_me_order_(first_me_order), prev_entry_(prev_entry), next_entry_(next_entry) {}
+    MEOrdersAtPrice(Side side, Price price,  MEOrder *first_me_order, MEOrdersAtPrice *prev_entry, MEOrdersAtPrice *next_entry)
+      : side_(side), price_(price), first_me_order_(first_me_order), prev_entry_(prev_entry), next_entry_(next_entry) {}
 
-  auto toString() const {
-    std::stringstream ss;
-    ss << "MEOrdersAtPrice["
-       << "side:" << sideToString(side_) << " "
-       
-  }
+    auto toString() const {
+      std::stringstream ss;
+      ss << "MEOrdersAtPrice["
+         << "side:" << sideToString(side_) << " "
+         << "price:" << priceToString(price_) << " "
+         << "first_me_order:" << (first_me_order_ ? first_me_order_->toString() : "null") << " "
+         << "prev:" << priceToString(prev_entry_ ? prev_entry_->price_ : Price_INVALID) << " "
+         << "next:" << priceToString(next_entry_ ? next_entry_->price_ : Price_INVALID) << "]";
+
+      return ss.str();
+    }
+  };
+
+  typedef std::array<MEOrdersAtPrice *, ME_MAX_PRICE_LEVELS> OrdersAtPriceHashMap;
+}
