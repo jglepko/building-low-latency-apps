7c562b67ed744bc758b71ca4ce74e76795c306b1
jglepko <36523615+jglepko@users.noreply.github.com>
2025-02-25 12:07:42 -0500

    Update me_order.h

--- Chapter6/exchange/matcher/me_order.h
+++ Chapter6/exchange/matcher/me_order.h
@@ -27,3 +27,29 @@ namespace Exchange {
         : ticker_id_(ticker_id), client_id_(client_id), client_order_id_(client_order_id), market_order_id_(market_order_id),
           side_(side), price_(price), qty_(qty), priority_(priority), prev_order_(prev_order), next_order_(next_order) {}
 
+    auto toString() const -> std::string;
+};
+
+typedef std::array<MEOrder *, ME_MAX_ORDER_IDS> OrderHashMap;
+typedef std::array<OrderHashMap, ME_MAX_NUM_CLIENTS> ClientOrderHashMap;
+
+struct MEOrdersAtPrice {
+  Side side_ = Side::INVALID;
+  Price price_ = Price_INVALID;
+
+  MEOrder *first_me_order_ = nullptr;
+
+  MEOrdersAtPrice *prev_entry_ = nullptr;
+  MEOrdersAtPrice *next_entry_ = nullptr;
+
+  MEOrdersAtPrice() = default;
+
+  MEOrdersAtPrice(Side side, Price price,  MEOrder *first_me_order, MEOrdersAtPrice *prev_entry, MEOrdersAtPrice *next_entry)
+    : side_(side), price_(price), first_me_order_(first_me_order), prev_entry_(prev_entry), next_entry_(next_entry) {}
+
+  auto toString() const {
+    std::stringstream ss;
+    ss << "MEOrdersAtPrice["
+       << "side:" << sideToString(side_) << " "
+       
+  }
