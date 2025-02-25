43aac5d9c9e3deb546cdba088475de77c7312bf0
jglepko <36523615+jglepko@users.noreply.github.com>
2025-02-25 11:40:02 -0500

    Update market_update.h

--- Chapter6/exchange/market_data/market_update.h
+++ Chapter6/exchange/market_data/market_update.h
@@ -3,60 +3,56 @@
 #include <sstream>
 
 #include "common/types.h"
-#include "common/lf_queue.h"
 
 using namespace Common;
 
 namespace Exchange {
 #pragma pack(push, 1)
-  enum class ClientResponseType : uint8_t {
+  enum class MarketUpdateType : uint8_t {
     INVALID = 0,
-    ACCEPTED = 1,
-    CANCELED = 2,
-    FILLED = 3,
-    CANCEL_REJECTED = 4
+    ADD = 1,
+    MODIFY = 2,
+    CANCEL = 3,
+    TRADE = 4
   };
 
-  inline std::string clientResponseTypeToString(ClientResponseType type) {
+  inline std::string marketUpdateTypeToString(MarketUpdateType type) {
     switch (type) {
-      case ClientResponseType::ACCEPTED:
-        return "ACCEPTED";
-      case ClientResponseType::CANCELED:
-        return "CANCELED";
-      case ClientResponseType::FILLED:
-        return "FILLED";
-      case ClientResponseType::CANCEL_REJECTED:
-        return "CANCEL_REJECTED";
-      case ClientResponseType::INVALID:
+      case MarketUpdateType::ADD:
+        return "ADD";
+      case MarketUpdateType::MODIFY:
+        return "MODIFY";
+      case MarketUpdateType::CANCEL:
+        return "CANCEL";
+      case MarketUpdateType::TRADE:
+        return "TRADE";
+      case MarketUpdateType::INVALID:
         return "INVALID";
     }
     return "UNKNOWN";
   }
 
-  struct MEClientResponse {
-    ClientResponseType type_ = ClientResponseType::INVALID;
-    ClientId client_id_ = ClientId_INVALID;
+  struct MEMarketUpdate {
+    MarketUpdateType type_ = MarketUpdateType::INVALID;
+  
+    OrderId order_id_ = OrderId_INVALID;
     TickerId ticker_id_ = TickerId_INVALID;
-    OrderId client_order_id_ = OrderId_INVALID;
-    OrderId market_order_id_ = OrderId_INVALID;
     Side side_ = Side::INVALID;
     Price price_ = Price_INVALID;
-    Qty exec_qty_ = Qty_INVALID;
-    Qty leaves_qty_ = Qty_INVALID;
+    Qty qty_ = Qty_INVALID;
+    Priority priority_ = Priority_INVALID;
 
     auto toString() const {
       std::stringstream ss;
-      ss << "MEClientResponse"
+      ss << "MEMarketUpdate"
          << " ["
-         << "type:" << clientResponseTypeToString(type_)
-         << " client:" << clientIdToString(client_id_)
+         << "type:" << marketUpdateTypeToString(type_)
          << " ticker:" << tickerIdToString(ticker_id_)
-         << " coid:" << orderIdToString(client_order_id_)
-         << " moid:" << orderIdToString(market_order_id_)
+         << " oid:" << orderIdToString(order_id_)
          << " side:" << sideToString(side_)
-         << " exec_qty:" << qtyToString(exec_qty_)
-         << " leaves_qty:" << qtyToString(leaves_qty_)
+         << " qty:" << qtyToString(qty_)
          << " price:" << priceToString(price_)
+         << " priority:" << priorityToString(priority_)
          << "]";
     return ss.str();
     }
@@ -64,5 +60,5 @@ namespace Exchange {
 
 #pragma pack(pop)
 
-  typedef LFQueue<MEClientResponse> ClientResponseLFQueue;
+  typedef Common::LFQueue<Exchange::MEMarketUpdate> MEMarketUpdateLFQueue;
 }
