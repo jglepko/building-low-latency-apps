f3164cac59c14e3be8bf665be5b017fa0afc7108
jglepko <36523615+jglepko@users.noreply.github.com>
2025-02-26 10:32:19 -0500

    Update exchange_main.cpp

--- Chapter6/exchange/exchange_main.cpp
+++ Chapter6/exchange/exchange_main.cpp
@@ -7,4 +7,27 @@ Exchange::MatchingEngine* matching_engine = nullptr;
 
 void signal_handler(int) {
   using namespace std::literals::chrono_literals;
-  std::this
+  std::this_thread::sleep_for(10s);
+
+  delete logger; logger = nullptr;
+  delete matching_engine; matching_engine = nullptr;
+
+  std::this_thread::sleep_for(10s);
+
+  exit(EXIT_SUCCESS);
+}
+
+int main(int, char **) {
+  logger = new Common::Logger("exchange_main.log");
+
+  std::signal(SIGINT, signal_handler);
+
+  const int sleep_time = 100 * 1000;
+
+  Exchange::ClientRequestLFQueue client_requests(ME_MAX_CLIENT_UPDATES);
+  Exchange::ClientResponseLFQueue client_responses(ME_MAX_CLIENT_UPDATES);
+  Exchange::MEMarketUpdateLFQueue market_updates(ME_MAX_MARKET_UPDATES);
+
+  std::string time_str;
+
+  logger->log("%:% %() % Starting Matching Engine...\n", __FILE__, __LINE__, __FUNCTION__, 
