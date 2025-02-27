0fa64412e9b74c4cd2ef0c4dd56ee9c2b6d202f2
jglepko <36523615+jglepko@users.noreply.github.com>
2025-02-27 14:34:52 -0500

    Update me_order_book.cpp

--- Chapter6/exchange/matcher/me_order_book.cpp
+++ Chapter6/exchange/matcher/me_order_book.cpp
@@ -21,6 +21,69 @@ namespace Exchange {
 
   auto MEOrderBook::match(TickerId ticker_id, ClientId client_id, Side side, OrderId client_order_id, OrderId new_market_order_id,
                           MEOrder* itr, Qty* leaves_qty) noexcept {
+    const auto order = itr;
+    const auto order_qty = order->qty_;
+    const auto fill_qty = std::min(*leaves_qty, order_qty);
 
+    *leaves_qty -= fill_qty;
+    order->qty_ -= fill_qty;
+
+    client_response_ = {ClientResponseType::FILLED, client_id, ticker_id, client_order_id,
+                        new_market_order_id, side, itr->price_, fill_qty, *leaves_qty_};
+    matching_engine_->sendClientResponse(&client_response_);
+
+    market_update_ = {MarketUpdateType::TRADE, OrderId_INVALID, ticker_id, side, itr->price_, fill_qty, Priority_INVALID};
+    matching_engine_->sendMarketUpdate(&market_update_);
+
+    if (!order->qty_) {
+      market_update_ = {MarketUpdateType::CANCEL, order->market_order_id_, ticker_id, order->side_,
+                        order->price_, order_qty, Priority_INVALID};
+      matching_engine_->sendMarketUpdate(&market_update_);
+
+      removeOrder(order);
+    } else {
+      market_update_ = {MarketUpdateType::MODIFY, order->market_order_id_, ticker_id, order->side_,
+                        order->price_, order->qty_, order->priority_};
+      matching_engine_->sendMarketUpdate(&market_update_);
+    }
+  }
+
+  auto MEOrderBook::checkForMatch(ClientId client_id, OrderId client_order_id, TickerId ticker_id, Side side,
+                                  Price price, Qty qty, Qty new_market_order_id) noexcept {
+    auto leaves_qty = qty;
+
+    if (side == Side::BUY) {
+      while (leaves_qty && asks_by_price_) {
+        const auto ask_itr = asks_by_price_->first_me_order_;
+        if (LIKELY(price < ask_itr->price_)) {
+          break;
+        }
+
+        match(ticker_Id, client_id, side, client_order_id, new_market_order_id, ask_itr, &leaves_qty);
+      }
+    }
+    if (side == Side::SELL) {
+      while (leaves_qty && bids_by_price_) {
+        const auto bid_itr = bids_by_price_->first_me_order;
+        if (LIKELY(price > bid_itr->price_)) {
+          break;
+        }
+
+        match(ticker_id, client_id, side, client_order_id, new_market_order_id, bid_itr, &leaves_qty);
+      }
+    }
+
+    return leaves_qty;
+  }
+
+  auto MEOrderBook::add(ClientId client_id, OrderId client_order_id, TickerId ticker_id, Side side,
+                                  Price price, Qty qty) noexcept -> void {
+    const auto new_market_order_id = generateNewMarketOrderId();
+    client_response_ = {ClientResponseType::ACCEPTED, client_id, ticker_id, client_order_id, new_market_order_id, 
+                        side, price, 0, qty};
+    matching_engine_->sendClientResponse(&client_response_);
+
+    const auto leaves_qty = checkForMatch(client_id, client_order_id, ticker_id, side, 
+                                          price, qty, new_market_order_id);
 
   }
