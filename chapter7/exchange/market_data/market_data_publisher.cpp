b76c17a25a458d9c0d86df89ae7f69c673ee7bcf
jglepko <36523615+jglepko@users.noreply.github.com>
2025-03-19 12:03:43 -0400

    Update market_data_publisher.cpp

--- chapter7/exchange/market_data/market_data_publisher.cpp
+++ chapter7/exchange/market_data/market_data_publisher.cpp
@@ -16,6 +16,22 @@ namespace Exchange {
     while (run_) {
       for (auto market_update = outgoing_md_updates_->getNextToRead();
            outgoing_md_updates_->size() && market_update; market_update = outgoing_md_udpates_->getNextToRead()) {
-        logger_.log("%:% %() %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_));
+        logger_.log("%:% %() % Sending seq:% %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_), next_inc_seq_num_,
+                    market_update->toString().c_str());
 
+        incremental_socket_.send(&next_inc_seq_num_, sizeof(next_inc_seq_num_));
+        incremental_socket_.send(market_update, sizeof(MEMarketUpdate));
+        outgoing_md_updates_->updateReadIndex();
+
+        auto next_write = snapshot_md_updates_.getNextToWrite();
+        next_write->seq_num_ = next_inc_seq_num_;
+        next_write->me_market_update_ = *market_update;
+        snapshot_md_udpates_.updateWriteIndex();
+
+        ++next_inc_seq_num_;
+      }
+
+      incremental_socket_.sendAndRecv();
+    }
+  }
 }
