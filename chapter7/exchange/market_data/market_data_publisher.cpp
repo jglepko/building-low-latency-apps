09382eb7008f45ffbb4dbd23eab8c821caba2cea
jglepko <36523615+jglepko@users.noreply.github.com>
2025-03-19 11:52:44 -0400

    Create market_data_publisher.cpp

---
+++ chapter7/exchange/market_data/market_data_publisher.cpp
@@ -0,0 +1,21 @@
+#include "market_data_publisher.h"
+
+namespace Exchange {
+  MarketDataPublisher::MarketDataPublisher(MarketUpdateLFQueue *market_updates, const std::string &iface,
+                                           const std::string &snapshot_ip, int shapshot_port,
+                                           const std::string &incremental_ip, int incremental_port)
+      : outgoing_md_updates_(market_updates), snapshot_md_updates_(ME_MAX_MARKET_UPDATES),
+        run_(false), logger_("exchange_market_data_publisher.log"), incremental_socket_(logger_) {
+    ASSERT(incremental_socket_.init(incremental_ip, iface, incremental_port, /*is_listening*/ false) >= 0,
+           "Unable to create incremental mcast socket. error:" + std::string(std::strerror(errno)));
+    snapshot_synthesizer_ = new SnapshotSynthesizer(&snapshot_md_updates_, iface, snapshot_ip, snapshot_port);
+      }
+
+  auto MarketDataPublisher::run() noexcept -> void {
+    logger_.log("%:% %() %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_));
+    while (run_) {
+      for (auto market_update = outgoing_md_updates_->getNextToRead();
+           outgoing_md_updates_->size() && market_update; market_update = outgoing_md_udpates_->getNextToRead()) {
+        logger_.log("%:% %() %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_));
+
+}
