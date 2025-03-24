7f4b846a8bc48d53e851817722406ba2183a019e
jglepko <36523615+jglepko@users.noreply.github.com>
2025-03-24 11:16:53 -0400

    Update market_order_book.h

--- chapter8/trading/strategy/market_order_book.h
+++ chapter8/trading/strategy/market_order_book.h
@@ -50,19 +50,165 @@ namespace Trading {
       }
     }
 
+    auto getBBO() const noexcept -> const BBO* {
+      return &bbo_;
+    }
 
+    auto toString(bool detailed, bool validity_check) const -> std::string;
 
-    MEOrderBook() = delete;
+    MarketOrderBook() = delete;
 
-    MEOrderBook(const MEOrderBook &) = delete;
+    MarketOrderBook(const MarketOrderBook &) = delete;
 
-    MEOrderBook(const MEOrderBook &&) = delete;
+    MarketOrderBook(const MarketOrderBook &&) = delete;
 
-    MEOrderBook &operator=(const MEOrderBook &) = delete;
+    MarketOrderBook &operator=(const MarketOrderBook &) = delete;
 
-    MEOrderBook &operator=(const MEOrderBook &&) = delete;
+    MarketOrderBook &operator=(const MarketOrderBook &&) = delete;
 
   private:
-    TickerId ticker_id_ = TickerId_INVALID;
+    const TickerId ticker_id_;
 
-    MatchingEngine *matching_engine_ = nullptr;
+    TradeEngine *trade_engine_ = nullptr;
+
+    OrderHashMap oid_to_order_;
+
+    MemPool<MarketOrdersAtPrice> orders_at_price_pool_;
+    MarketOrdersAtPrice *bids_by_price_ = nullptr;
+    MarketOrdersAtPrice *asks_by_price_ = nullptr;
+
+    OrdersAtPriceHashMap price_orders_at_price_;
+
+    MemPool<MarketOrder> order_pool_;
+
+    BBO bbo_;
+
+    std::string time_str_;
+    Logger *logger_ = nullptr;
+
+  private:
+    auto priceToIndex(Price price) const noexcept {
+      return (price % ME_MAX_PRICE_LEVELS);
+    }
+
+    auto getOrdersAtPrice(Price price) const noexcept -> MarketOrdersAtPrice * {
+      return price_orders_at_price_.at(priceToIndex(price));
+    }
+
+    auto addOrdersAtPrice(MEOrdersAtPrice *new_orders_at_price) noexcept {
+      price_orders_at_price_.at(priceToIndex(new_orders_at_price->price_)) = new_orders_at_price;
+
+      const auto best_orders_by_price = (new_orders_at_price->side_ == Side::BUY ? bids_by_price_ : asks_by_price_);
+      if (UNLIKELY(!best_orders_by_price)) {
+        (new_orders_at_price->side_ == Side::BUY ? bids_by_price_ : asks_by_price_) + new_orders_at_price;
+        new_orders_at_price->prev_entry_ = new_orders_at_price->next_entry_ = new_orders_at_price;
+      } else {
+        auto target = best_orders_by_price;
+        bool add_after = ((new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ > target->price_) ||
+                          (new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ < target->price_));
+        if (add_after) {
+          target = target->next_entry_;
+          add_after = ((new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ > target->price_) ||
+                          (new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ < target->price_));
+          if (add_after)
+            target = target->next_entry_;
+        }
+        while (add_after && target != best_orders_by_price) {
+          add_after = ((new_orders_at_price->side_ == Side::SELL && new_orders_at_price->price_ > target->price_) ||
+                          (new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ < target->price_));
+           if (add_after)
+            target = target->next_entry_;
+        }
+
+        if (add_after) { // add new_orders_at_price after target
+          if (target == best_orders_by_price) {
+            target = best_orders_by_price->prev_entry_;
+          }
+          new_orders_at_price->prev_entry_ = target;
+          target->next_entry_->prev_entry_ = new_orders_at_price;
+          new_orders_at_price->next_entry_ = target->next_entry_;
+          target->next_entry_ = new_orders_at_price;
+        } else { // add new_orders_at_price before target
+          new_orders_at_price->prev_entry_ = target->prev_entry_;
+          new_orders_at_price->next_entry_ = target;
+          target->prev_entry_->next_entry_ = new_orders_at_price;
+          target->prev_entry_ = new_orders_at_price;
+
+          if ((new_orders_at_price->side_ == Side::BUY && new_orders_at_price->price_ > best_orders_by_price->price_) ||
+              (new_orders_at_price->side_ == Side::SELL && 
+               new_orders_at_price->price_ < best_orders_by_price->price_)) {
+            target->next_entry_ = (target->next_entry_ == best_orders_by_price ? new_orders_at_price 
+                                                                               : target->next_entry_);
+            (new_orders_at_price->side_ == Side::BUY ? bids_by_price_ : asks_by_price_) = new_order_at_price;
+          }
+        }
+      }
+    }
+
+    auto removeOrdersAtPrice(Side side, Price price) noexcept {
+      const auto best_orders_by_price = (side == Side::BUY ? bids_by_price_ : asks_by_price_);
+      auto orders_at_price = getOrdersAtPrice(price);
+
+      if (UNLIKELY(orders_at_price->next_entry_ == orders_at_price)) { // empty side of book.
+        (side == Side::BUY ? bids_by_price_ : asks_by_price_) = nullptr;
+      } else {
+        orders_at_price->prev_entry_->next_entry_ = orders_at_price->next_entry_;
+        orders_at_price->next_entry_->prev_entry_ = orders_at_price->prev_entry_;
+
+        if (orders_at_price == best_orders_by_price) { 
+          (side == Side::BUY ? bids_by_price_ : asks_by_price_) = orders_at_price->next_entry_;
+        }
+
+        orders_at_price->prev_entry_ = orders_at_price->next_entry_ = nullptr;
+      }
+
+      price_orders_at_price_.at(price_to_index(price)) = nullptr;
+
+      orders_at_price_pool_.deallocate(orders_at_price);
+    }
+
+    auto removeOrder(MEOrder *order) noexcept {
+      auto orders_at_price = getOrdersAtPrice(order->price_);
+
+      if (order->prev_order_ == order) { // only one element.
+        removeOrdersAtPrice(order->side_, order->price_);
+      } else { // remove the link.
+        const auto order_before = order->prev_order_;
+        const auto order_after = order->next_order_;
+        order_before->next_order_ = order_after;
+        order_after->prev_order_ = order_before;
+
+        if (orders_at_price->first_me_order_ == order) {
+          orders_at_price->first_me_order_ = order_after;
+        }
+
+        order->prev_order_ = order->next_order_ = nullptr;
+      }
+
+      oid_to_order_.at(order->order_id_) = nullptr;
+      order_pool_.deallocate(order);
+    }
+
+     auto addOrder(MarketOrder *order) noexcept -> void {
+      const auto orders_at_price = getOrdersAtPrice(order->price_);
+
+      if (!orders_at_price) {
+        order->next_order_ = order->prev_order_ = order;
+
+        auto new_orders_at_price = orders_at_price_pool_.allocate(order->side_, order->price_, order, nullptr, nullptr);
+        addOrdersAtPrice(new_orders_at_price);
+      } else {
+        auto first_order = (orders_at_price ? orders_at_price->first_mkt_order_ : nullptr);
+
+        first_order->prev_order_->next_order_ = order;
+        order->prev_order_ = first_order->prev_order_;
+        order->next_order_ = first_order;
+        first_order->prev_order_ = order;
+      }
+
+      oid_to_order_.at(order->order_id_) = order;
+    }
+  };
+
+  typedef std::array<MarketOrderBook *, ME_MAX_TICKERS> MarketOrderBookHashMap;
+}
