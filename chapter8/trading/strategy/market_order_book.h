fb416c662215be71afa41433e81616b5904b45c6
jglepko <36523615+jglepko@users.noreply.github.com>
2025-03-24 10:38:16 -0400

    Update market_order_book.h

--- chapter8/trading/strategy/market_order_book.h
+++ chapter8/trading/strategy/market_order_book.h
@@ -7,3 +7,62 @@
 #include "market_order.h"
 #include "exchange/market_data/market_update.h"
 
+namespace Trading {
+  class TradeEngine;
+
+  class MarketOrderBook final {
+  public:
+    MarketOrderBook(TickerId ticker_id, Logger *logger);
+
+    ~MarketOrderBook();
+
+    auto onMarketUpdate(const Exchange::MEMarketUpdate *market_update) noexcept -> void;
+
+    auto setTradeEngine(TradeEngine *trade_engine) {
+      trade_engine_ = trade_engine;
+    }
+
+    auto updateBBO(bool update_bid, bool update_ask) noexcept {
+      if(update_bid) {
+        if(bids_by_price_) {
+          bbo_.bid_price_ = bids_by_price_->price_;
+          bbo_.bid_qty_ = bids_by_price_->first_mkt_order_->qty_;
+          for (auto order = bids_by_price_->first_mkt_order_->next_order_; order != bids_by_price_->first_mkt_order_; order = order->next_order_)
+            bbo_.bid_qty_ += order->qty_;
+        }
+        else {
+          bbo_.bid_price_ = Price_INVALID;
+          bbo_.bid_qty_ = Qty_INVALID;
+        }
+      }
+
+      if(update_ask) {
+        if(asks_by_price_) {
+          bbo_.ask_price_ = asks_by_price_->price_;
+          bbo_.ask_qty_ = asks_by_price_->first_mkt_order_->qty_;
+          for (auto order = asks_by_price_->first_mkt_order_->next_order_; order != asks_by_price_->first_mkt_order_; order = order->next_order_)
+            bbo_.ask_qty_ += order->qty_;
+        }
+        else {
+          bbo_.ask_price_ = Price_INVALID;
+          bbo_.ask_qty_ = Qty_INVALID;
+        }
+      }
+    }
+
+
+
+    MEOrderBook() = delete;
+
+    MEOrderBook(const MEOrderBook &) = delete;
+
+    MEOrderBook(const MEOrderBook &&) = delete;
+
+    MEOrderBook &operator=(const MEOrderBook &) = delete;
+
+    MEOrderBook &operator=(const MEOrderBook &&) = delete;
+
+  private:
+    TickerId ticker_id_ = TickerId_INVALID;
+
+    MatchingEngine *matching_engine_ = nullptr;
