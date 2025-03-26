fa3a779bb4c4fedc62cf7831560faded369f55ae
jglepko <36523615+jglepko@users.noreply.github.com>
2025-03-26 14:34:52 -0400

    Update position_keeper.h

--- chapter9/trading/strategy/position_keeper.h
+++ chapter9/trading/strategy/position_keeper.h
@@ -50,3 +50,106 @@ namespace Trading {
         real_pnl_ += std::min(static_cast<int32_t>(client_response->exec_qty_), std::abs(old_position)) *
                      (opp_side_vwap - client_response->price_) * sideToValue(client_response->side_);
         if (position_ * old_position < 0) { // flipped position to opposite sign
+          open_vwap_[side_index] = (client_response->price_ * std::abs(position_));
+          open_vwap_[opp_side_index] = 0;
+        }
+      }
+
+      if (!position_) { // flat
+        open_vwap_[sideToIndex(Side::BUY)] = open_vwap_[sideToIndex(Side::SELL)] = 0;
+        unreal_pnl_ = 0;
+      } else {
+        if (position_ > 0)
+          unreal_pnl_ = 
+            (client_response->price_ - open_vwap_[sideToIndex(Side::BUY)] / std::abs(position_)) *
+            std::abs(position_);
+        else
+          unreal_pnl_ = 
+            (open_vwap_[sideToIndex(Side::SELL)] / std::abs(position_) - client_response->price_) *
+            std::abs(position_);
+      }
+
+        total_pnl_ = unreal_pnl_ + real_pnl_;
+
+        std::string time_str_;
+        logger_->log("%:% %() % % %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_), 
+                     toString(), client_response->toString().c_str());
+    }
+
+    auto updateBBO(const BBO *bbo, Logger *logger) noexcept {
+      std::string time_str;
+      bbo_ = bbo;
+
+      if (position_ && bbo->bid_price_ != Price_INVALID && bbo->ask_price_ != Price_INVALID) {
+        const auto mid_price = (bbo->bid_price_ + bbo->ask_price_) * 0.5;
+        if (position_ > 0)
+          unreal_pnl_ = 
+            (mid_price - open_vwap_[sideToIndex(Side::BUY)] / std::abs(position_)) *
+            std::abs(position_);
+        else
+          unreal_pnl_ = 
+            (open_vwap_[sideToIndex(Side::SELL)] / std::abs(position_) - mid_price) *
+            std::abs(position_);
+
+        const auto old_total_pnl = total_pnl_;
+        total_pnl = unreal_pnl_ + real_pnl_;
+
+        if (total_pnl_ != old_total_pnl)
+          logger_->log("%:% %() % % %\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_), 
+                     toString(), bbo->toString());
+      }
+    }
+  };
+
+  class PositionKeeper {
+  public:
+    PositionKeeper(Common::Logger *logger)
+        : logger_(logger) {
+    }
+
+    PositionKeeper() = delete;
+
+    PositionKeeper(const PositionKeeper &) = delete;
+
+    PositionKeeper(const PositionKeeper &&) = delete;
+
+    PositionKeeper &operator=(const PositionKeeper &) = delete;
+
+    PositionKeeper &operator=(const PositionKeeper &&) = delete;
+
+  private:
+    std::string time_str_;
+    Common::Logger *logger_ = nullptr;
+
+    std::array<PositionInfo, ME_MAX_TICKERS> ticker_position_;
+
+  public:
+    auto addFill(const Exchange::MEClientResponse *client_response) noexcept {
+      ticker_position_.at(client_response->ticker_id_).addFill(client_response, logger_);
+    }
+
+    auto updateBBO(TickerId ticker_id, const BBO *bbo) noexcept {
+      ticker_position_.at(ticker_id).updateBBO(bbo, logger_);
+    }
+
+    auto getPositionInfo(TickerId ticker_id) const noexcept {
+      return &(ticker_position_.at(ticker_id));
+    }
+
+    auto toString() const {
+      double total_pnl = 0;
+      Qty total_vol = 0;
+
+      std::stringstream ss;
+      for(TickerId i = 0; i < ticker_position_.size(); ++i) {
+        ss << "TickerId:" << tickerIdToString(i) << " " << ticker_position_.at(i).toString() << "\n";
+
+        total_pnl += ticker_position_.at(i).total_pnl_;
+        total_vol += ticker_position_.at(i).volume_;
+      }
+      ss << "Total PnL:" << total_pnl << " Vol:" << total_vol << "\n";
+
+      return ss.str();
+    }
+  };
+}
