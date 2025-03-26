91225e7f8fb30378a6818df0cd342cfc3ecbbf61
jglepko <36523615+jglepko@users.noreply.github.com>
2025-03-26 15:22:29 -0400

    Update om_order.h

--- chapter9/trading/strategy/om_order.h
+++ chapter9/trading/strategy/om_order.h
@@ -19,57 +19,42 @@ namespace Trading {
     switch (side) {
       case OMOrderState::PENDING_NEW:
         return "NEW";
-      
-      case ClientRequestType::INVALID:
+      case OMOrderState::LIVE:
+        return "LIVE";
+      case OMOrderState::PENDING_CANCEL:
+        return "PENDING_CANCEL";
+      case OMOrderState::DEAD:
+        return "DEAD";
+      case OMOrderState::INVALID:
         return "INVALID";
     }
     return "UNKNOWN";
   }
 
-#pragma pack(push, 1)
-
   struct MEClientRequest {
-    ClientRequestType type_ = ClientRequestType::INVALID;
-
-    ClientId client_id_ = ClientId_INVALID;
     TickerId ticker_id_ = TickerId_INVALID;
     Orderid order_id_ = OrderId_INVALID;
     Side side_ = Side::INVALID;
     Price price_ = Price_INVALID;
     Qty qty_ = Qty_INVALID;
+    OMOrderState order_state_ = OMOrderState::INVALID;
 
     auto toString() const {
       std::stringstream ss;
-      ss << "MEClientRequest"
+      ss << "OMOrder" 
          << " ["
-         << "type:" << clientRequestTypeToString(type_)
-         << "client:" << clientIdToString(client_id_)
-         << " ticker:" << tickerIdToString(ticker_id_)
+         << " tid:" << tickerIdToString(ticker_id_)
          << " oid:" << orderIdToString(order_id_)
          << " side:" << sideToString(side_)
-         << " qty:" << qtyToString(qty_)
          << " price:" << priceToString(price_)
+         << " qty:" << qtyToString(qty_)
+         << " state:" << OMOrderStateToString(order_state)
          << "]";
-    return ss.str();
+      
+     return ss.str();
     }
   };
 
-  struct OMClientRequest {
-    size_t seq_num_ = 0;
-    MEClientRequest me_client_request_;
-
-    auto toString() const {
-      std::stringstream ss;
-      ss << "OMClientRequest"
-         << " ["
-         << " seq:" << seq_num_
-         << " " << me_client_request_.toString()
-         << "]";
-      return ss.str();
-    }
-  };
-
-#pragma pack(pop)
-
-  typedef LFQueue<MEClientRequest> ClientRequestLFQueue;
+  typedef std::array<OMOrder, sideToIndex(Side::MAX) + 1> OMOrderSideHashMap;
+  typedef std::array<OMOrderSideHashMap, ME_MAX_TICKERS> OMOrderTickerSideHashMap;
 }
